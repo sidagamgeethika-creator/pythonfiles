0f3a139eaaa32794d355635b0a36f2bfd0c9f6dd
Sidagam Geethika <sidagamgeethika96@gmail.com>
2026-09-24 12:08:05 +0530

    Add files via upload

---
+++ variable and rules.py
@@ -0,0 +1,151 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#VARIABLES
+print(3+9)
+12
+a=10
+print(a)
+10
+x=50
+print(x)
+50
+print(X)
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    print(X)
+NameError: name 'X' is not defined. Did you mean: 'x'?
+Z=100
+print(Z)
+100
+3=100
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+a3=100
+print(a3)
+100
+5x=9
+SyntaxError: invalid decimal literal
+a0123456789=100
+print(a0123456789)
+100
+name="geetha"
+print(name)
+geetha
+print("name")
+name
+city=vija
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    city=vija
+NameError: name 'vija' is not defined
+city"vija"
+SyntaxError: invalid syntax
+city="vij"
+print(city)
+vij
+a=8
+b=2
+print(a+b)
+10
+fname="geetha"
+lname="sida"
+print(fname+lname)
+geethasida
+print(fname+" "+lname)
+geetha sida
+print(fname,lname)
+geetha sida
+a=3,b=7
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+a=4;b=7
+print(a+b)
+11
+a,b=4,7
+print(a+b)
+11
+a=3
+b=8
+print(a+b)
+11
+@=10
+SyntaxError: invalid syntax
+$=2
+SyntaxError: invalid syntax
+_=30
+print(_)
+30
+_a=100
+print(_a)
+100
+if=20
+SyntaxError: invalid syntax
+while=6
+SyntaxError: invalid syntax
+a=1,2,3,4,5,6,7
+print(a)
+(1, 2, 3, 4, 5, 6, 7)
+a,b,c=1,2,2
+print(a,b,c)
+1 2 2
+a,b,c=1,2,3,45,
+Traceback (most recent call last):
+  File "<pyshell#50>", line 1, in <module>
+    a,b,c=1,2,3,45,
+ValueError: too many values to unpack (expected 3, got 4)
+first name="geetha"
+SyntaxError: invalid syntax
+first_name"geetha"
+SyntaxError: invalid syntax
+first_name="geetha"
+pront(first_name)
+Traceback (most recent call last):
+  File "<pyshell#54>", line 1, in <module>
+    pront(first_name)
+NameError: name 'pront' is not defined. Did you mean: 'print'?
+print(first_name)
+geetha
+>>> firstname=geethaa
+Traceback (most recent call last):
+  File "<pyshell#56>", line 1, in <module>
+    firstname=geethaa
+NameError: name 'geethaa' is not defined
+>>> firstname="geetha"
+>>> print(firstname)
+geetha
+>>> a=(1,2,3)
+>>> print(a)
+(1, 2, 3)
+>>> a,b,c=(5,6,7)
+>>> print(a,b,c)
+5 6 7
+>>> a=90
+>>> print(a)
+90
+>>> del a
+>>> print(a)
+Traceback (most recent call last):
+  File "<pyshell#67>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined. Did you mean: 'a3'?
+>>> name="geetha"
+>>> print(name)
+geetha
+>>> print(NAME)
+Traceback (most recent call last):
+  File "<pyshell#70>", line 1, in <module>
+    print(NAME)
+NameError: name 'NAME' is not defined
+>>> NAME="geetha"
+>>> print(name)
+geetha
+>>> print(NAME)
+geetha
+>>> a,b,c=10
+Traceback (most recent call last):
+  File "<pyshell#74>", line 1, in <module>
+    a,b,c=10
+TypeError: cannot unpack non-iterable int object
+>>> a=b=c=10
+>>> print
+<built-in function print>
+>>> print(a,b,c)
+10 10 10
